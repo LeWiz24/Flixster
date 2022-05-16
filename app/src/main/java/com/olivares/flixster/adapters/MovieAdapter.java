e9caa82d02a896e129956b2f664046970dbe6546
Mario Olivares <MarioOlivares_95@hotmail.com>
2022-05-15 20:40:50 -0400

    Added alternative image on landscape using alternative resources

--- app/src/main/java/com/olivares/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/olivares/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.olivares.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,7 +67,17 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+            // if phone is in landscape
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // then imageUrl = backdropimage
+                imageUrl = movie.getBackdropPath();
+            } else {
+                // else imageUrl = posterimage
+                imageUrl = movie.getPosterPath();
+            }
+            // else imageUrl = posterimage
+            Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 }
